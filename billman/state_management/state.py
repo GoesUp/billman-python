fb872ec5821924372f21a80b4cf679bd8f8c0ff4
mkajt <kajtna.maja@gmail.com>
2020-11-07 12:37:54 +0100

    Creating module Bill and some examples.

--- billman/state_management/state.py
+++ billman/state_management/state.py
@@ -1,11 +1,28 @@
 from pydantic import BaseModel
 from typing import List
+# from datetime import datetime
+#from pydantic.schema import datetime
 
 
 class User(BaseModel):
     name: str
     surname: str
 
+class Bill(BaseModel):
+    category: str
+    reference: str
+    date_payment: str
+    date_due: str
+    total: float
+    purpose: str
+    code_purpose: str
+    recipient_and_address: str
+    BIC_bank_recipient: str
+    IBAN_recipient: str
+    visible_family: bool
+
 
 class State(BaseModel):
     users: List[User]
+    bills: List[Bill]
+
