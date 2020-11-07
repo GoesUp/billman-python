dfe95a0cf4b2e3a23c098930ce441f917d76eefc
barbara7777 <badobe.lipnik@gmail.com>
2020-11-07 12:06:57 +0100

    Added Category, User and Community models

--- billman/state_management/get_state.py
+++ billman/state_management/get_state.py
@@ -1,12 +1,53 @@
 from functools import lru_cache
-from billman.state_management.state import State, User
+from billman.state_management.state import *
+
 
 state = State(
     users=[
         User(
+            id=0,
             name="Gregor",
             surname="Gabrovšek",
+            address="Čokoladna ulica nekje",
+            bancAccNumber="SI56293847932865923",
+            localCredit="3.1",
+            family=Family(relations={1, 10})
+        ),
+        User(
+            id=1,
+            name="Miki",
+            surname="Miška",
+            address="Disneyland 16a",
+            bancAccNumber="SI563548463245356",
+            localCredit="8327947197",
+            family=Family(relations={0})
+        ),
+        User(
+            id=2,
+            name="Mojca",
+            surname="Pokrajculja",
+            address="Lonček 113",
+            bancAccNumber="SI563453636865923",
+            localCredit="50.50",
+            family=Family(relations=set())
         )
+    ],
+    categories=[
+        Category(name="Family"),
+        Category(name="Food"),
+        Category(name="Fun"),
+        Category(name="Education"),
+        Category(name="Sport"),
+        Category(name="Transport"),
+        Category(name="Fixed expenses"),
+        Category(name="Community"),
+        Category(name="Other"),
+    ],
+    community=[
+        Community(id=0, amount=10.5, cause="Družini vidmajer je toča uničila streho. Pomagajte!"),
+        Community(id=0, cause="Omogočimo otrokom izlet v Črno goro!"),
+        Community(id=0, cause="Zbiramo sredstva za male živali..."),
+        Community(id=0, amount=1054.52, cause="Gregor, Maja in Barbara so lačni. Darujte!"),
     ]
 )
 
--- billman/state_management/state.py
+++ billman/state_management/state.py
@@ -1,11 +1,32 @@
 from pydantic import BaseModel
-from typing import List
+from typing import List, Set
+
+
+class Family(BaseModel):
+    relations: Set[int] = None
 
 
 class User(BaseModel):
+    id: int
     name: str
     surname: str
+    address: str
+    bancAccNumber: str
+    localCredit: float
+    family: Family
+
+
+class Category(BaseModel):
+    name: str
+
+
+class Community(BaseModel):
+    id: int
+    amount: float = 0.0
+    cause: str
 
 
 class State(BaseModel):
     users: List[User]
+    categories: List[Category]
+    community: List[Community]
