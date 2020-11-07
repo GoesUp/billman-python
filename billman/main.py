62638709cf1f1a48b3dbbfd8e0df068530bf6b66
barbara7777 <badobe.lipnik@gmail.com>
2020-11-07 15:04:19 +0100

    zvezdice :)

--- billman/main.py
+++ billman/main.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
 import uvicorn
 from billman.base.base import router as base_router
+from fastapi.middleware.cors import CORSMiddleware
 
 
 def run() -> None:
     app = FastAPI()
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
     app.include_router(base_router)
     uvicorn.run(app, host="127.0.0.1", port=5000, log_level="info")
 
