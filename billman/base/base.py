95e7c1a1d5b805cda1ba37f3ad15e88ecd628655
barbara7777 <badobe.lipnik@gmail.com>
2020-11-07 17:02:29 +0100

    idk

--- billman/base/base.py
+++ billman/base/base.py
@@ -1,8 +1,7 @@
 from fastapi.params import Depends
 from fastapi.routing import APIRouter
-from billman.state_management.get_state import get_state
-from billman.state_management.state import State
-
+from billman.state_management.get_state import get_state, List
+from billman.state_management.state import State, Bill, Community, User, Family, Category
 
 router = APIRouter()
 
@@ -10,3 +9,71 @@ router = APIRouter()
 @router.get("/")
 async def test(state: State = Depends(get_state)) -> State:
     return state
+
+
+@router.get("/users")
+def all_users(state: State = Depends(get_state)) -> List[User]:
+    return state.users
+
+
+@router.get("/bills")
+def all_bills(state: State = Depends(get_state)) -> List[Bill]:
+    return state.bills
+
+
+@router.get("/categories")
+def all_categories(state: State = Depends(get_state)) -> List[Category]:
+    return state.categories
+
+
+@router.get("/user/{user_id}")
+def get_user_by_id(user_id: int, state: State = Depends(get_state)) -> User:
+    for user in state.users:
+        if user.id == user_id:
+            return user
+
+
+@router.get("/user/{user_id}/family")
+def get_user_family(user_id: int, state: State = Depends(get_state)) -> Family:
+    for user in state.users:
+        if user.id == user_id:
+            return user.family
+
+
+@router.get("/user/{user_id}/bills")
+def get_bills_for_user_id(user_id: int, state: State = Depends(get_state)) -> List[Bill]:
+    user_bills = []
+    for bill in state.bills:
+        if bill.id_payer == user_id:
+            user_bills.append(bill)
+    user_bills.sort(key=lambda x: x.recipient, reverse=False)
+    return user_bills
+
+
+@router.get("/user/{user_id}/bills")
+def get_bills_for_user_id(user_id: int, state: State = Depends(get_state)) -> List[Bill]:
+    user_bills = []
+    for bill in state.bills:
+        if bill.id_payer == user_id:
+            user_bills.append(bill)
+    return user_bills
+
+
+@router.get("/community/active")
+def get_active_community(state: State = Depends(get_state)) -> List[Community]:
+    community = []
+    for com in state.community:
+        if com.collected < com.goal:
+            community.append(com)
+    return community
+
+
+@router.get("/community/closed")
+def get_active_community(state: State = Depends(get_state)) -> List[Community]:
+    community = []
+    for com in state.community:
+        if com.collected >= com.goal:
+            community.append(com)
+    return community
+
+
